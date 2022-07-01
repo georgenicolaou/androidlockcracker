4e791cee7f52461a862861eec13730f95a582daa
Dovine Owuor <70941806+Dovineowuor@users.noreply.github.com>
2022-07-01 21:34:41 +0300

    Update androidlockcracker.py

--- androidlockcracker.py
+++ androidlockcracker.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 """
 AndroidLockCracker - Cracking and generating Android lock hashes
-Copyright (C) 2022   George Nicolaou and Dovine Owuor(george({at})silensec({dot})com)
+Copyright (C) 2022   George Nicolaou (george({at})silensec({dot})com) and Dovine Owuor
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
